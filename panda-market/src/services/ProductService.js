ebb6093ec8ddf032cfad06d27dd27f0e19cd79c3
김민희 <minhee@MinBookAir.local>
2025-01-24 22:36:07 +0900

    temp

--- panda-market/src/services/ProductService.js
+++ panda-market/src/services/ProductService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
-  // baseURL: "https://panda-market-t9vr.onrender.com",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://panda-market-t9vr.onrender.com",
 });
 
 const productService = {
